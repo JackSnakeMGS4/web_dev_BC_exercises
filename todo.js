8d505562222c570d2a2e76e207e1939d48a9924f
Jaime Rivas <jaimerivas81@outlook.com>
2018-10-03 15:15:57 -0700

    improvement for to do list

--- todo.js
+++ todo.js
@@ -3,18 +3,45 @@ setTimeout(function()
 {
 	var input = prompt("What will you do?");
 
+	function list()
+	{
+		console.log("**********");
+		todos.forEach(function(todo, index)
+		{
+			console.log(index + ": " + todo);
+		});
+		console.log("**********");
+	}
+
+	function add()
+	{
+		let todoToAdd = prompt("What are you adding?");
+		todos.push(todoToAdd);
+	}
+
+	function deleteTodo()
+	{
+		let index = prompt("Delete index: ex. 0");
+		todos.splice(index, 1);
+	}
+
 	while(input !== "quit")
 	{
 		if(input === "list")
 		{
-			console.log(todos);
+			list();
 		}
 		else if(input === "new")
 		{
-			let todoToAdd = prompt("What are you adding?");
-			todos.push(todoToAdd);
+			add();
+		}
+		else if(input === "delete")
+		{
+			deleteTodo();
 		}
 		input = prompt("What will you do?");
 	}
 	console.log("Quiting App");
 }, 500);
+
+	
